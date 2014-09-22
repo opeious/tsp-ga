6b0459d2d77ba667a93c93f33f14e582fbb6b80b
Tapan Ranjan <tapanranjan@gmail.com>
2014-09-23 02:27:53 +0530

    deleting extra files
    Cleanup

--- js/game.js
+++ js/game.js
@@ -1,29 +1,18 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update, render: render  });
-var boids;
-var boidsCG;
-var boidImg;
-var i;
-var conStr;
+var cities;
+var cityScale = 0.5;
 
 function preload() {
 
-    game.load.image('boid', 'img/boid.png');
+    game.load.image('city', 'img/city.png');
 }
 
 function create() {
-    boids = game.add.group();
-    
-    //game.physics.enable(image, Phaser.Physics.ARCADE   ->> enables physics for image
-    
-    
-    
+    cities = game.add.group();
+   
     this.game.time.advancedTiming = true;
     this.fpsText = this.game.add.text(10, 10, '',       //fps tracking
     { font: '8px Arial', fill: '#ffffff' });
-    
-    
-    
-  
 }
 
 function update()
@@ -31,118 +20,54 @@ function update()
     if (this.game.time.fps !== 0) {
         this.fpsText.setText(this.game.time.fps + ' FPS');   //update fps tracker
     }
-    
-    
-    if(boids.countLiving()<slider1Val)                          //boid population control
+    if(cities.countLiving()<slider1Val)
     {
-        addBoid(game.world.randomX,game.world.randomY);
-            boids.setAll('scale.x',0.5);
-            boids.setAll('scale.y',0.5);
-    
+        addCity(game.world.randomX,game.world.randomY);
+        cities.setAll('scale.x',cityScale);
+        cities.setAll('scale.y',cityScale);
     }
-    while(boids.countLiving()>slider1Val){
-        var a = boids.getFirstAlive();
-        if(a)
-         a.kill();
-        
+    while(cities.countLiving()>slider1Val)
+    {
+        var killCity = cities.getFirstAlive();
+        if(killCity)
+         killCity.kill();
     }
-    
-
-    
 }
 
 function render()
 {
 }
 
-
-var addBoid = function(x,y)
+var addCity = funciton(x,y)
 {
-    var boid = boids.getFirstDead();
+    var city = cities.getFirstDead();
     
-    if(boid == null)
+    if(city == null)
     {
-        boid = new Boid(game);
-        boids.add(boid);
+        city = new City(game);
+        cities.add(city);
     }
     
-    boid.revive();
+    city.revive();
     
-    boid.x = x;
-    boid.y = y;
-    boid.body.collideWorldBounds = true;
+    city.x = x;
+    city.y = y;
     
-    return boid;
+    return city;
 }
 
-
-var Boid = function(game, x, y)
+var City = function(game, x,y)
 {
-     Phaser.Sprite.call(this, game, x, y, 'boid');
-
-    // Set the pivot point for this sprite to the center
-    this.anchor.setTo(0.5, 0.5);
-
-    // Enable physics on the boids
-    this.game.physics.enable(this, Phaser.Physics.ARCADE);
-
-    // Define constants that affect motion
-    this.SPEED = 100; // missile speed pixels/second
-    this.TURN_RATE = 5; // turn rate in degrees/frame
-    //this.AVOID_DISTANCE = 30; // pixels  
+    Phaser.Sprite.call(this,game,x,y,'city');
+    this.anchor.setTo(0.5,0.5);
+    
+    this.game.physics.enable(this,Phaser.Physics.ARCADE);
 }
 
-Boid.prototype = Object.create(Phaser.Sprite.prototype);   //Boid is a type of sprite
-Boid.prototype.constructor = Boid;
+City.prototype = Object.create(Phaser.Sprite.prototype);
+City.prototype.constructor = City;
 
-Boid.prototype.update = function() {
+City.prototype.update = function() {
     if(!this.alive)
         return;
-    
-    
-    var targetAngle
-    // Calculate the angle from the missile to the mouse cursor game.input.x
-    // and game.input.y are the mouse position; substitute with whatever
-    // target coordinates you need.
-    if(slider2Val==0){
-        targetAngle = this.game.math.angleBetween(
-        this.x, this.y,
-        this.game.width/2, this.game.height/2
-        );
-    }
-    else
-    {
-        //targetAngle = this.game.physics.arcade.angleToPointer(Boid);
-            targetAngle = this.game.math.angleBetween(
-        this.x, this.y,
-        game.input.x, game.input.y
-        );
-    }
-    
-    // Gradually (this.TURN_RATE) aim the boids towards the target angle
-    if (this.rotation !== targetAngle) {
-        // Calculate difference between the current angle and targetAngle
-        var delta = targetAngle - this.rotation;
-
-        // Keep it in range from -180 to 180 to make the most efficient turns.
-        if (delta > Math.PI) delta -= Math.PI * 2;
-        if (delta < -Math.PI) delta += Math.PI * 2;
-
-        if (delta > 0) {
-            // Turn clockwise
-            this.angle += this.TURN_RATE;
-        } else {
-            // Turn counter-clockwise
-            this.angle -= this.TURN_RATE;
-        }
-
-        // Just set angle to target angle if they are close
-        if (Math.abs(delta) < this.game.math.degToRad(this.TURN_RATE)) {
-            this.rotation = targetAngle;
-        }
-    }
-
-    // Calculate velocity vector based on this.rotation and this.SPEED
-    this.body.velocity.x = Math.cos(this.rotation) * this.SPEED;
-    this.body.velocity.y = Math.sin(this.rotation) * this.SPEED;    
 }
